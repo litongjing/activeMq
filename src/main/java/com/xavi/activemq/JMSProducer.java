d2f199eb30922f27eba0e816d1636bb759c989d2
xavi <491135051@qq.com>
2017-05-30 00:34:54 +0800

    123

--- src/main/java/com/xavi/activemq/JMSProducer.java
+++ src/main/java/com/xavi/activemq/JMSProducer.java
@@ -48,7 +48,7 @@ public class JMSProducer {
     }
 
     public static void main(String[] args) {
-        //1112345
+        //11123457
         System.out.println(System.getProperty("java.class.path"));//
         //连接工厂
         ConnectionFactory connectionFactory;
