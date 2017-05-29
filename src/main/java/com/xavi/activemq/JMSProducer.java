d299c10518960c116d8bc2ef36c552db5d87afaa
xavi <491135051@qq.com>
2017-05-30 00:14:22 +0800

    something happend

--- src/main/java/com/xavi/activemq/JMSProducer.java
+++ src/main/java/com/xavi/activemq/JMSProducer.java
@@ -48,6 +48,7 @@ public class JMSProducer {
     }
 
     public static void main(String[] args) {
+        //111
         System.out.println(System.getProperty("java.class.path"));//
         //连接工厂
         ConnectionFactory connectionFactory;
