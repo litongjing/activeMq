d1a73e65537fed007ebe4b98005c338eb0f7964c
xavi <491135051@qq.com>
2017-05-30 12:26:20 +0800

    Merge branch '1-branch' of https://github.com/litongjing/activeMq into 1-branch
    # Conflicts:
    #	src/main/java/com/xavi/activemq/JMSProducer.java

--- src/main/java/com/xavi/activemq/JMSProducer.java
+++ src/main/java/com/xavi/activemq/JMSProducer.java
@@ -52,6 +52,7 @@ public class JMSProducer {
         //123ab
         //2
         //675758588
+        //12345
         //cabcdefg1abc
         System.out.println(System.getProperty("java.class.path"));//
         //连接工厂
